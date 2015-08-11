7f1a98e157cafb75f0a2c1a70021553edcbab1f7
Rafael Rinaldi <rafael@rinaldi.io>
2015-08-11 17:34:49 -0300

    Moved test files to their own folder.

---
+++ test/test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var injectHTML = require('./');
+var test = require('tape');
+var hasElement = require('./test/util/hasElement');
+var fixture = '<button class="c-button">Click me!</button>';
+
+
+test('inject html', function(t) {
+
+  t.equal(hasElement('.c-button'), false, 'DOM should not contain fixture');
+
+  injectHTML(fixture);
+
+  t.equal(hasElement('.c-button'), true, 'DOM should contain fixture');
+  t.end();
+
+});
---
+++ test/util/hasElement.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = function(selector) {
+  return document.querySelector(selector) !== null;
+};
