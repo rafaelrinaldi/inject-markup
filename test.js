25caef3686fb4feacd87076e0679aeb9b3532c50
Rafael Rinaldi <rafael@rinaldi.io>
2015-08-11 17:58:22 -0300

    Added test file to the root.

---
+++ test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var injectHTML = require('./');
+var test = require('tape');
+var fixture = '<button class="c-button">Click me!</button>';
+
+function hasElement(selector) {
+  return document.querySelector(selector) !== null;
+}
+
+test('inject html', function(t) {
+
+  var container;
+  var now;
+
+  t.equal(hasElement('.c-button'), false, 'DOM should not contain fixture before calling `innerHTML()`');
+
+  now = +new Date();
+  container = injectHTML(fixture);
+
+  t.equal(hasElement('.c-button'), true, 'DOM should contain fixture after calling `innerHTML()`');
+  t.equal(container.innerHTML, fixture, 'Container markup should match fixture chunk');
+  t.equal(container.getAttribute('id'), 'fixture-' + now, 'Fixture id should be composed by prefix plus timestamp');
+
+  t.end();
+
+});
