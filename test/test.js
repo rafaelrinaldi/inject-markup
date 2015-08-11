a26a593a1e76b732e74b4e4913517c5762a067ef
Rafael Rinaldi <rafael@rinaldi.io>
2015-08-11 17:51:58 -0300

    Finished writing test suite.

--- test/test.js
+++ test/test.js
@@ -5,14 +5,20 @@ var test = require('tape');
 var hasElement = require('./util/hasElement');
 var fixture = '<button class="c-button">Click me!</button>';
 
-
 test('inject html', function(t) {
 
-  t.equal(hasElement('.c-button'), false, 'DOM should not contain fixture');
+  var container;
+  var now;
 
-  injectHTML(fixture);
+  t.equal(hasElement('.c-button'), false, 'DOM should not contain fixture before calling `innerHTML()`');
 
-  t.equal(hasElement('.c-button'), true, 'DOM should contain fixture');
+  now = +new Date();
+  container = injectHTML(fixture);
+
+  t.equal(hasElement('.c-button'), true, 'DOM should contain fixture after calling `innerHTML()`');
+  t.equal(container.innerHTML, fixture, 'Container markup should match fixture chunk');
+  t.equal(container.getAttribute('id'), 'fixture-' + now, 'Fixture id should be composed by prefix plus timestamp');
+
   t.end();
 
 });
